2905839cd9545d773907d1554330a457fcc63276
XanderVi <engineermexanic@gmail.com>
2018-08-02 17:38:16 +0300

    now you can add youtube videos

--- posts/models.py
+++ posts/models.py
@@ -2,16 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    id_post = models.AutoField(unique=True, primary_key=True)
     #id_user = models.CharField(max_length=200, null=False, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, null=True, blank=True)
     #preview = models.CharField(max_length=500)
-    text = models.TextField()
-    youtube_link = models.CharField(max_length=200, null=True, blank=True)
-    date_create = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    id_post = models.AutoField(unique=True, primary_key=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    youtube_link = models.CharField(max_length=200, blank=True, null=True)
+    date_create = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
